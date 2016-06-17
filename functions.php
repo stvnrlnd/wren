c821ee8587969864ec7cc09b47f66fed150197b0
Steven Roland <steven@mojave.io>
2016-06-17 14:46:45 -0400

    Main menu
    - Created the main navigation

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+  add_theme_support( 'menus' );
+
   function wren_theme_styles() {
     wp_enqueue_style( 'font_css', 'https://fonts.googleapis.com/css?family=Lato' );
     wp_enqueue_style( 'bootstrap_css', get_template_directory_uri().'/css/bootstrap.css' );
@@ -12,5 +14,10 @@
     wp_enqueue_script( 'wren_js', get_template_directory_uri().'/js/wren.js', array('jquery', 'bootstrap_js'), '', true );
   }
   add_action( 'wp_enqueue_scripts', 'wren_theme_scripts' );
+
+  function register_theme_menus() {
+    register_nav_menus( array( 'primary-menu' => __( 'Primary Menu' ) ) );
+  }
+  add_action('init', 'register_theme_menus');
 
 ?>
--- header.php
+++ header.php
@@ -9,13 +9,13 @@
     <?php wp_head(); ?>
 
   </head>
-  <body>
+  <body <?php body_class(); ?>>
     <div>
       <header>
         <nav class="navbar navbar-default navbar-fixed-top">
           <div class="container-fluid">
             <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#primary-menu-collapse" aria-expanded="false">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -24,20 +24,15 @@
               <a class="navbar-brand" href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
             </div>
 
-            <div class="collapse navbar-collapse" id="navbar-collapse">
-              <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Link</a></li>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                  <ul class="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="#">Separated link</a></li>
-                  </ul>
-                </li>
-              </ul>
+            <div class="collapse navbar-collapse" id="primary-menu-collapse">
+              <?php
+                $defaults = array(
+                  'container' => false,
+                  'theme_location' => 'primary-menu',
+                  'menu_class' => 'nav navbar-nav navbar-right'
+                );
+                wp_nav_menu( $defaults );
+              ?>
             </div>
           </div>
         </nav>
