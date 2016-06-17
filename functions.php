229107a35f55c2b10bdb398456c3df28fdbe033c
Steven Roland <steven@mojave.io>
2016-06-17 12:50:42 -0400

    Fixed issue with functions.php not saving correctly
    - Had a moment, was saving to the wrong repo v_v

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+  function wren_theme_styles() {
+    wp_enqueue_style( 'font_css', 'https://fonts.googleapis.com/css?family=Lato' );
+    wp_enqueue_style( 'bootstrap_css', get_template_directory_uri().'/css/bootstrap.css' );
+    wp_enqueue_style( 'main_css', get_template_directory_uri().'/style.css' );
+  }
+  add_action( 'wp_enqueue_scripts', 'wren_theme_styles' );
+
+  function wren_theme_scripts() {
+    wp_enqueue_script( 'bootstrap_js', get_template_directory_uri().'/js/bootstrap.min.js', array('jquery'), '', true );
+    wp_enqueue_script( 'wren_js', get_template_directory_uri().'/js/wren.js', array('jquery', 'bootstrap_js'), '', true );
+  }
+  add_action( 'wp_enqueue_scripts', 'wren_theme_scripts' );
+
+?>
--- header.php
+++ header.php
@@ -1,3 +1,12 @@
-<p>
-  Header is working...
-</p>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php wp_title(); ?></title>
+
+    <?php wp_head(); ?>
+
+  </head>
+  <body>
