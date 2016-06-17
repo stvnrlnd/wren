f9a9b5b7b24ba8f21b227832f5cb7025ec6bb936
Steven Roland <steven@mojave.io>
2016-06-17 13:50:57 -0400

    Get the posts!
    - Now posts, if there are any, are dynamically displayed.

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php get_header(); ?>
 
-<h1>Hello, beautiful!</h1>
+<div class="container">
+  <main>
+    <section>
+      <?php if( have_posts() ) : while( have_posts() ) : the_post();  ?>
+        <h1 class="text-center"><?php the_title(); ?></h1>
+        <p>
+          <?php the_content(); ?>
+        </p>
+      <?php endwhile; else : ?>
+        <p>
+          <?php _e( 'Yikes! There aren\'t any posts to show right now!' ); ?>
+        </p>
+      <?php endif; ?>
+    </section>
+  </main>
+</div>
 
 <?php get_footer(); ?>
